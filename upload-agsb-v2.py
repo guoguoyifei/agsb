7fd9da33e28ba7c4c60ee582074a56f2639f3fd4
guoguoyifei <162897128+guoguoyifei@users.noreply.github.com>
2025-06-06 00:34:53 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -33,7 +33,7 @@ CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的�
 # ====== 全局可配置参数（可直接在此处修改） ======
 USER_NAME = "guoguoyi"         # 用户名
 UUID = "316984f3-a453-4cb5-a0b8-ded21395806c"                     # UUID，留空则自动生成
-PORT = 49999                   # Vmess端口，留空或0则自动生成
+PORT = 21669                   # Vmess端口，留空或0则自动生成
 DOMAIN = "gg.guoguoyi.dpdns.org"                   # 域名，留空则自动获取
 CF_TOKEN = "IDEfNM4RTaGMQ5fcDAIu4okZD-1PzzYZueiMRK34"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
